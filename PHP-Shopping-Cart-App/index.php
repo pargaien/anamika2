4677e2e0ecfe037e94179c6624dc659d60d5d428
Katherine Hambley <superdesigngirl@mac.com>
2014-05-20 10:00:23 -0500

    created title and copyright statement in index.php

--- PHP-Shopping-Cart-App/index.php
+++ PHP-Shopping-Cart-App/index.php
@@ -1,5 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Buy MiniFigs</title>
+</head>
+<body>
+    <h1>Buy Lego MiniFigures</h1>
+    <p>&copy; <?php echo date('Y'); ?> BuyMiniFigs.com</p>
+</body>
+</html>
 
-echo "Hello World!";
-
-?>
